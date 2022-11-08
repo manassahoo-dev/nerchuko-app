c3e0f237cf865fd63750adac2e7a9875907beafc
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2022-11-08 23:28:13 +0530

    Button Component

--- frontend/components/atoms/Button.tsx
+++ frontend/components/atoms/Button.tsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React from 'react';
 
-interface ButtonProps {
-  primary?: boolean;
-  backgroundColor?: string;
-  size?: "small" | "medium" | "large";
-  label: string;
-  onClick?: () => void;
+interface Props {
+  border: string;
+  color: string;
+  children?: React.ReactNode;
+  height: string;
+  onClick: () => void;
+  radius: string;
+  width: string;
 }
 
-export const Button = ({
-  primary = false,
-  size = "medium",
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+const Button: React.FC<Props> = ({
+  border,
+  color,
+  children,
+  height,
+  onClick,
+  radius,
+  width,
+}) => {
   return (
     <button
-      type="button"
-      className={[
-        "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
-        `storybook-button--${size}`,
-        mode,
-      ].join(" ")}
-      style={{ backgroundColor }}
-      {...props}
+      onClick={onClick}
+      style={{
+        backgroundColor: color,
+        border,
+        borderRadius: radius,
+        height,
+        width,
+      }}
     >
-      {label}
+      {children}
     </button>
   );
 };
+
+export default Button;
