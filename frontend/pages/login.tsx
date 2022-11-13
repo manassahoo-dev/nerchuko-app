401026ed13e1ae0b7bd6da93b74402a798e6c7ce
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2022-11-13 23:52:12 +0530

    Update Text

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -24,7 +24,7 @@ export default function Login(): JSX.Element {
                 src='https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg'
                 alt='logo'
               />
-              Flowbite
+              Nerchuko
             </a>
             <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
               <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
